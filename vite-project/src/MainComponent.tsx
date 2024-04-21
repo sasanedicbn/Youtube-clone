dca8cba0ad578503794b8ca56153a868930e04e7
Sasa Nedic <svetasmak0@gmail.com>
2024-04-22 01:23:26 +0200

    Change code

--- vite-project/src/MainComponent.tsx
+++ vite-project/src/MainComponent.tsx
@@ -1,32 +1,7 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+
 
 const MainComponent = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://youtube-v31.p.rapidapi.com/captions',
-        params: {
-          part: 'snippet',
-          videoId: 'M7FIvfx5J10'
-        },
-        headers: {
-          'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-          'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-        }
-      };
 
-      try {
-        const response = await axios.request(options);
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div>
@@ -34,7 +9,7 @@ const MainComponent = () => {
         <h1>New<span>video</span></h1>
       </div>
       <div className="current-videos">
-        <p>DADASDASASDASDASDASDAdadasdasSDSDAASD</p>
+       
       </div>
     </div>
   );
---
+++ vite-project/src/https.tsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+export  const playListVideos = async () => {
+          const options = {
+            method: 'GET',
+            url: 'https://youtube-v31.p.rapidapi.com/playlistItems',
+            params: {
+              playlistId: 'RDZiQo7nAkQHU',
+              part: 'snippet',
+              maxResults: '50'
+            },
+            headers: {
+              'X-RapidAPI-Key': "853339e32dmsh484669df916fa96p1fa47cjsn9dbd231055af",
+              'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+            }
+          };
+          
+          try {
+            const response = await axios.request(options);
+            console.log(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+          playListVideos()
+        };
+    
+       
+      
