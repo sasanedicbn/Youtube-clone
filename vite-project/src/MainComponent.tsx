4ac890872096d1492da65219d9d481c16a325af4
Sasa Nedic <svetasmak0@gmail.com>
2024-04-21 23:48:50 +0200

    Add axios for fetch api

--- vite-project/src/MainComponent.tsx
+++ vite-project/src/MainComponent.tsx
@@ -1,13 +1,43 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 const MainComponent = () => {
-    return(
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://youtube-v31.p.rapidapi.com/captions',
+        params: {
+          part: 'snippet',
+          videoId: 'M7FIvfx5J10'
+        },
+        headers: {
+          'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+          'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
       <div>
-        <div>
-            <h1>New<span>video</span></h1>
-        </div>
-        <div className="current-videos">
-          <p>DADASDASASDASDASDASDAdadasdasSDSDAASD</p>
-        </div>
+        <h1>New<span>video</span></h1>
       </div>
-    )
-}
+      <div className="current-videos">
+        <p>DADASDASASDASDASDASDAdadasdasSDSDAASD</p>
+      </div>
+    </div>
+  );
+};
+
 export default MainComponent;
