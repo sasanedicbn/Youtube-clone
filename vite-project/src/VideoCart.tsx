042d6749a9305bdb750172bd5a96f741c36cb6ae
Sasa Nedic <svetasmak0@gmail.com>
2024-04-23 21:17:23 +0200

    Fix some error with date

--- vite-project/src/VideoCart.tsx
+++ vite-project/src/VideoCart.tsx
@@ -1,8 +1,9 @@
 
 const VideoCard = ({ video }) => {
-    const { title, thumbnails, publishedTime } = video.snippet;
+    const { title, thumbnails, publishTime } = video.snippet;
+  
 
-    const formattedPublishedTime = new Date(publishedTime).toLocaleDateString('en-US', {
+    const formattedPublishedTime = new Date(publishTime).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
         day: 'numeric'
@@ -10,7 +11,7 @@ const VideoCard = ({ video }) => {
 
     return (
         <div className="video-card">
-            <img src={thumbnails.default.url} alt={title} />
+            <img src={thumbnails.medium.url} alt={title} />
             <h3>{title}</h3>
             <p>{formattedPublishedTime}</p>
         </div>
