05badc62cc46f5f9d20a94747ddd81d136b52575
Sasa Nedic <svetasmak0@gmail.com>
2024-04-26 15:06:52 +0200

    Clean code

--- vite-project/src/SingleVideo.tsx
+++ vite-project/src/SingleVideo.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { fetchVideoDetails } from "./https";
 import { useParams } from "react-router";
+import { FaThumbsUp, FaShareSquare, FaDownload } from 'react-icons/fa'; // Import ikonica
 
 const SingleVideo = () => {
  const [videoDetails, setVideoDetails] = useState(null)
   const {id} = useParams()
- 
   console.log(videoDetails)
-
     useEffect(() => {
         const fetchDetails = async () => {
             try {
@@ -20,17 +19,39 @@ const SingleVideo = () => {
         };
         fetchDetails()
     }, [id]);
+
     if (!videoDetails) {
-        // If videoDetails is still null, render a loading state or return null
         return <div>Loading...</div>;
     }
-    const {snippet, statistics,} = videoDetails.items[0]
+    const {snippet, statistics} = videoDetails.items[0]
+    const viewCount = parseInt(statistics.viewCount);
+    console.log(snippet)
+    console.log(statistics)
     return (
         <div>
-            SASA SINGLE VIDEO
+            <div>
+              VIDEO KLIP OVDJE
+            </div>
+            <div className="video-description">
+              <h4>{snippet.title}</h4>
+              <div>
+                <p className="viewers">{viewCount.toLocaleString()} views</p>
+                <p className="likes">
+                  <FaThumbsUp /> {statistics.likeCount} 
+                  <span>
+                    <FaShareSquare /> Share
+                  </span> 
+                  <span >
+                    <FaDownload /> Download
+                  </span>
+                </p>
+              </div>
+            </div>
         </div>
     );
 };
 
 export default SingleVideo;
+
+
 
