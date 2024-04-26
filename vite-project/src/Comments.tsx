5f274859ac9d3a61179cb5d44060f5b6ecc45a82
Sasa Nedic <svetasmak0@gmail.com>
2024-04-26 16:14:23 +0200

    Create comments component

--- vite-project/src/Comments.tsx
+++ vite-project/src/Comments.tsx
@@ -1,7 +1,21 @@
-const Comments = () => {
-    return(
+import React from "react";
 
-    )
-}
+const Comments = ({ comments }) => {
+    return (
+        <div className="comments">
+            {comments.items.map(comment => {
+                const { snippet } = comment.snippet.topLevelComment;
+                return (
+                    <div key={comment.id}>
+                        <img src={snippet.authorProfileImageUrl} alt="User Avatar" />
+                        <p>{snippet.authorDisplayName}</p>
+                        <p>{snippet.publishedAt}</p>
+                        <p>{snippet.textOriginal}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default Comments;
