94869de1d29bbbfc72c69f8b63a8ce429d9aba3d
Sasa Nedic <svetasmak0@gmail.com>
2024-04-29 21:12:50 +0200

    Create chagnes

--- vite-project/src/MainComponent.tsx
+++ vite-project/src/MainComponent.tsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { fetchVideosList } from "./https";
 import VideoCart from "./VideoCart";
 
+const MainComponent = ({ clickedCategory, searchQuery }: { clickedCategory: any; searchQuery: string }) => {
+  const [videos, setVideos] = useState([]);
 
+  // Fetch videos based on search query
+  useEffect(() => {
+    const fetchSearchVideos = async () => {
+      const videosData = await fetchVideosList(searchQuery);
+      setVideos(videosData.items);
+    };
+    fetchSearchVideos();
+  }, [searchQuery]);
 
-const MainComponent = ({clickedCategory, searchQuery}:{clickedCategory:any, searchQuery: string}) => {
-    const [videos, setVideos] = useState([]);
-  
-    useEffect(() => {
-        const fetchPlaylistVideos = async () => {
-          let videosData 
-          if(searchQuery) {
-            videosData = await fetchVideosList(searchQuery)
-          } else{
-          videosData = await fetchVideosList(clickedCategory)
-        };
-        setVideos(videosData.items)}
-        fetchPlaylistVideos();
-      }, [searchQuery,clickedCategory]);
- 
+  // Fetch videos based on clicked category
+  useEffect(() => {
+    const fetchCategoryVideos = async () => {
+      const videosData = await fetchVideosList(clickedCategory);
+      setVideos(videosData.items);
+    };
+    fetchCategoryVideos();
+  }, [clickedCategory]);
 
   return (
     <div className="main-content">
       <div>
-       <h1>{clickedCategory}</h1>
+        <h1>{clickedCategory}</h1>
       </div>
       <div className="current-videos">
-        {videos.map((video,_) => (
-            <VideoCart key={video.id.videoId} video={video}/>
+        {videos.map((video, index) => (
+          <VideoCart key={index} video={video} />
         ))}
       </div>
     </div>
